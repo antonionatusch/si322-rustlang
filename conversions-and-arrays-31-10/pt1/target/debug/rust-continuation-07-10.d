8ce68830dc63810f7b7107d9b6565c4905370af3
Antonio Natusch <anthony2004951@gmail.com>
2024-11-04 14:57:34 -0400

    xd

--- conversions-and-arrays-31-10/pt1/target/debug/rust-continuation-07-10.d
+++ conversions-and-arrays-31-10/pt1/target/debug/rust-continuation-07-10.d
@@ -1,1 +1,1 @@
-/home/tusch/Desktop/rust-stuff/si322-rustlang/rust-continuation-07-10/target/debug/rust-continuation-07-10: /home/tusch/Desktop/rust-stuff/si322-rustlang/rust-continuation-07-10/src/main.rs
+/home/tusch/Desktop/rust-stuff/si322-rustlang/conversions-and-arrays-31-10/pt1/target/debug/rust-continuation-07-10: /home/tusch/Desktop/rust-stuff/si322-rustlang/conversions-and-arrays-31-10/pt1/src/main.rs
